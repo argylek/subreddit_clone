0daa626300172f9311e1d6055128cc6e0d62e625
Scott Key <scottdkey@gmail.com>
2020-02-20 10:44:22 -0700

    Index and show ready

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,13 +1,21 @@
 class SubsController < ApplicationController
+  before_action :set_sub, only: [:show, :edit, :update, :create, :destroy]
   def index
+    @subs = Sub.all
   end
 
   def show
+    # @sub = Sub.find(params[:id])
   end
 
   def edit
   end
 
   def new
+  end
+
+  private
+  def set_sub
+    @sub = Sub.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Sub.create(name:Faker::Artist.name)
+end
+Sub.create(name:'React')
+Sub.create(name:'Animals')
+Sub.create(name:'Javascript')
+Sub.create(name:'HTML')
+Sub.create(name:'CSS')
+Sub.create(name:'Angular JS')
+Sub.create(name:'Rails')
