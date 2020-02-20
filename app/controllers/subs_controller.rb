6a574bb479cfcb1b81fec89317a36b6e4c084b96
Scott Key <scottdkey@gmail.com>
2020-02-20 11:38:01 -0700

    Created topics sub route and model

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,5 +1,5 @@
 class SubsController < ApplicationController
-  before_action :set_sub, only: [:show, :edit, :update, :create, :destroy]
+  before_action :set_sub, only: [:show, :edit, :update, :destroy]
   def index
     @subs = Sub.all
   end
@@ -12,10 +12,24 @@ class SubsController < ApplicationController
   end
 
   def new
+    @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to subs_path
+    else
+      puts 'Error text field must have something'
+      render :new
+    end
   end
 
   private
   def set_sub
     @sub = Sub.find(params[:id])
+  end
+  def sub_params
+    params.require(:sub).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :subs
   root 'subs#index'
+
+  resources :subs do 
+    resources :topics
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
